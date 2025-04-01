074c73bcf279d6e76e86761ede970ebee59dd7af
Igor Gromadskiy <gromadskiyigor@gmail.com>
2025-04-01 13:32:52 +0300

    [Update] config

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/uk',
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
