fe2715bdeaa28b90ea3eed6f7ac319b9b63cfec9
Igor Gromadskiy <gromadskiyigor@gmail.com>
2025-04-01 13:04:09 +0300

    [Update] country code

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,9 @@ import { routing } from '@/i18n/routing';
 import { NextRequest, NextResponse } from 'next/server';
 
 function getLocaleFromRequest(request: NextRequest) {
-  const acceptLanguage = request.headers.get('accept-language') || '';
   const country = request.headers.get('x-country-code');
-  console.log('country:', country);
 
-  if (country === 'SK') return 'sk';
-  if (country === 'UA') return 'uk';
-
-  const languages = acceptLanguage
-    .split(',')
-    .map(lang => lang.split(';')[0].trim());
-  console.log('parsed languages:', languages);
-  if (languages.includes('sk')) return 'sk';
-  if (languages.includes('uk') || languages.includes('uk-UA')) return 'uk';
-
+  if (country === 'SK' || country === 'SI') return 'sk';
   return 'uk';
 }
 
@@ -24,11 +13,9 @@ const intlMiddleware = createMiddleware(routing);
 
 export default function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  console.log('pathname:', pathname);
 
   if (pathname === '/') {
     const locale = getLocaleFromRequest(request);
-    console.log('redirecting to:', `/${locale}`);
     return NextResponse.redirect(new URL(`/${locale}`, request.url));
   }
 
